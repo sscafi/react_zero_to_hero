799a990d05b03ea7360ce38331dd95a4fa400b1a
Saher Scafi <95544378+sscafi@users.noreply.github.com>
2024-08-29 05:52:05 -0400

    Add files via upload

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+// PascalCasing
+function Message(){
+    // JSX : JavaScript XML
+    const name  = 'Saher';
+    if (name)
+        return <h1> Hello {name} </h1>
+    return <h1> Hello Stranger</h1>
+}
+
+export default Message;
